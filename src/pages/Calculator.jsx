266376410255a69540a15f0b3a0da6c51d5e5f31
Elielson Nascimento <nascimento.elielson@gmail.com>
2022-02-15 01:09:43 -0300

    Finalizando a calculadora

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,30 +1,169 @@
-import React from 'react';
-import './style/Calculator.css';
+import React, { useState } from "react";
+import "./style/Calculator.css";
 
 function Calculator() {
+  const [num, setNum] = useState(0);
+  const [oldNum, setOldNum] = useState(0);
+  const [operation, setOperation] = useState();
+
+  function inputNum({ target: { value } }) {
+    if (num === 0) {
+      setNum(value);
+    } else {
+      setNum(num + value);
+    }
+  }
+
+  function clear() {
+    setNum(0);
+  }
+
+  function percentage() {
+    setNum(num / 100);
+  }
+
+  function changeSign() {
+    if (num > 0) {
+      setNum(-Math.abs(num));
+    } else {
+      setNum(Math.abs(num));
+    }
+  }
+
+  function operatorHandler ({ target: { value } }) {
+    setOperation(value);
+    setOldNum(num);
+    setNum(0)
+  }
+
+  function calculate () {
+    switch (operation) {
+      case '/':
+          setNum(oldNum / num);
+      break;
+      case 'X':
+          setNum(oldNum * num);
+      break;
+      case '-':
+          setNum(oldNum - num);
+      break;
+      case '+':
+          setNum(parseFloat(oldNum) + parseFloat(num));
+      break;
+      default:
+        setNum(0)
+        break;
+    }
+  }
+
   return (
-    <div className='Calculator'>
-      <button className='ButtonGray'>AC</button>
-      <button className='ButtonGray'>+/-</button>
-      <button className='ButtonGray'>%</button>
-      <button className='ButtonOrange'>/</button>
-      <button className='ButtonGrayDark'>7</button>
-      <button className='ButtonGrayDark'>8</button>
-      <button className='ButtonGrayDark'>9</button>
-      <button className='ButtonOrange'>X</button>
-      <button className='ButtonGrayDark'>4</button>
-      <button className='ButtonGrayDark'>5</button>
-      <button className='ButtonGrayDark'>6</button>
-      <button className='ButtonOrange'>-</button>
-      <button className='ButtonGrayDark'>1</button>
-      <button className='ButtonGrayDark'>2</button>
-      <button className='ButtonGrayDark'>3</button>
-      <button className='ButtonOrange'>+</button>
-      <button className='ButtonGrayDark' id='Zero'>0</button>
-      <button className='ButtonGrayDark'>,</button>
-      <button className='ButtonOrange'>=</button>
-    </div>
-  )
+    <main className="Align">
+      <div className="Calculator">
+        <h1 className="Result">{num}</h1>
+        <button className="ButtonGray" onClick={clear}>
+          AC
+        </button>
+        <button className="ButtonGray" onClick={changeSign}>
+          +/-
+        </button>
+        <button className="ButtonGray" onClick={percentage}>
+          %
+        </button>
+        <input
+          type="button"
+          className="ButtonOrange"
+          onClick={operatorHandler}
+          value="/"
+        />
+        <input
+          type="button"
+          className="ButtonGrayDark"
+          onClick={inputNum}
+          value="7"
+        />
+        <input
+          type="button"
+          className="ButtonGrayDark"
+          onClick={inputNum}
+          value="8"
+        />
+        <input
+          type="button"
+          className="ButtonGrayDark"
+          onClick={inputNum}
+          value="9"
+        />
+        <input
+          type="button"
+          className="ButtonOrange"
+          onClick={operatorHandler}
+          value="X"
+        />
+        <input
+          type="button"
+          className="ButtonGrayDark"
+          onClick={inputNum}
+          value="4"
+        />
+        <input
+          type="button"
+          className="ButtonGrayDark"
+          onClick={inputNum}
+          value="5"
+        />
+        <input
+          type="button"
+          className="ButtonGrayDark"
+          onClick={inputNum}
+          value="6"
+        />
+        <input
+          type="button"
+          className="ButtonOrange"
+          onClick={operatorHandler}
+          value="-"
+        />
+        <input
+          type="button"
+          className="ButtonGrayDark"
+          onClick={inputNum}
+          value="1"
+        />
+        <input
+          type="button"
+          className="ButtonGrayDark"
+          onClick={inputNum}
+          value="2"
+        />
+        <input
+          type="button"
+          className="ButtonGrayDark"
+          onClick={inputNum}
+          value="3"
+        />
+        <input
+          type="button"
+          className="ButtonOrange"
+          onClick={operatorHandler}
+          value="+"
+        />
+        <input
+          type="button"
+          className="ButtonGrayDark"
+          id="Zero"
+          onClick={inputNum}
+          value="0"
+        />
+        <input
+          type="button"
+          className="ButtonGrayDark"
+          onClick={inputNum}
+          value="."
+        />
+        <button className="ButtonOrange" onClick={calculate}>=</button>
+      </div>
+    </main>
+  );
 }
 
 export default Calculator;
